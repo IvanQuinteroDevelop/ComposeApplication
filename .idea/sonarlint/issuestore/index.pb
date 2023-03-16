010af3160d0dea726e19d2b675f2abe9e265b218
IvanQuinteroDevelop <ivanquinterodevelop@gmail.com>
2023-03-16 17:20:47 -0500

    ProgressBar.kt examples

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,9 +1,3 @@
 
 Å
 Qapp/src/main/java/com/example/composeapplication/components/basics/TextExample.kt,b/b/bb2e058556f680b76d919c2ad4de284e79caac56
-}
-Mapp/src/main/java/com/example/composeapplication/components/basics/Columns.kt,5/2/52d0be7ced76c333c9701d62abf2ec2c2ab00bee
-{
-Kapp/src/main/java/com/example/composeapplication/components/basics/Boxes.kt,8/b/8b653d7bc8535aac97979d99527fef03bbbbfa3c
-à
-Xapp/src/main/java/com/example/composeapplication/components/layouts/ConstraintExample.kt,d/7/d7ec543f908eed32e9f59196968a54aecd93e39a
---
+++ app/src/main/java/com/example/composeapplication/components/basics/ProgressBar.kt
@@ -0,0 +1,79 @@
+package com.example.composeapplication.components.basics
+
+import android.util.Log
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.LinearProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview(showSystemUi = true)
+@Composable
+fun MyProgress() {
+    var showLoading by rememberSaveable {
+        mutableStateOf(false)
+    }
+    Column(
+        Modifier
+            .padding(16.dp)
+            .fillMaxSize(), verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        if (showLoading) {
+            CircularProgressIndicator(color = Color.Magenta, strokeWidth = 8.dp)
+            LinearProgressIndicator(
+                Modifier.padding(top = 30.dp),
+                color = Color.Cyan,
+                backgroundColor = Color.Red
+            )
+        }
+
+        Button(onClick = { showLoading = !showLoading }) {
+            Text(text = "Cargar datos")
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun MyProgressAdvance() {
+    var progressValue by rememberSaveable {
+        mutableStateOf(0f)
+    }
+
+    Log.i("valor", "$progressValue")
+    Column(
+        Modifier
+            .padding(16.dp)
+            .fillMaxSize(), verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        LinearProgressIndicator(progressValue)
+
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            Button(onClick = {
+                if (progressValue >= 0.1f) progressValue -= 0.1f
+            }) { Text(text = "Reducir") }
+            Button(onClick = {
+                if (progressValue <= 1f) progressValue += 0.1f
+            }) {
+                Text(text = "Incrementar")
+            }
+        }
+    }
+}
