11ee53ebd85efa7782dbc6288d7dd1d89a5f2d5d
IvanQuinteroDevelop <ivanquinterodevelop@gmail.com>
2023-03-30 17:10:16 -0500

    horizontal recyclerview of SuperHero.kt

--- app/src/main/java/com/example/composeapplication/components/recycler/RecyclerViews.kt
+++ app/src/main/java/com/example/composeapplication/components/recycler/RecyclerViews.kt
@@ -1,16 +1,33 @@
 package com.example.composeapplication.components.recycler
 
+import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composeapplication.R
+import com.example.composeapplication.model.SuperHero
 
 @Preview(showSystemUi = true)
 @Composable
 fun SimpleRecyclerView() {
     val myList = listOf("Navi", "Aris", "Pepe", "Ivan")
-    LazyColumn{
+    LazyColumn {
         item { Text(text = "First item") }
         item { Text(text = "Second item") }
         item { Text(text = "Third item") }
@@ -22,4 +39,55 @@ fun SimpleRecyclerView() {
             Text(text = "Hello my name is $it")
         }
     }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun SuperHeroView() {
+    val context = LocalContext.current
+    LazyRow(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+        items(getSuperHero()) { superHero ->
+            ItemHero(superHero = superHero) {
+                Toast.makeText(context, it.superHeroName, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
+
+@Composable
+fun ItemHero(superHero: SuperHero, onItemSelected: (SuperHero) -> Unit) {
+    Card(border = BorderStroke(2.dp, Color.Red), modifier = Modifier
+        .width(200.dp).clickable { onItemSelected(superHero) }) {
+        Column() {
+            Image(
+                painter = painterResource(id = superHero.photo),
+                contentDescription = "SuperHero Avatar",
+                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = superHero.superHeroName,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+            Text(text = superHero.realName, fontSize = 12.sp,
+                modifier = Modifier.align(Alignment.CenterHorizontally))
+            Text(text = superHero.publisher,
+                fontSize = 10.sp,
+                modifier = Modifier
+                    .align(Alignment.End)
+                    .padding(6.dp))
+        }
+    }
+}
+
+fun getSuperHero(): List<SuperHero> {
+    return listOf(
+        SuperHero("Spiderman", "Petter Parker", "Marvel", R.drawable.spiderman),
+        SuperHero("Wolverine", "James Howlett", "Marvel", R.drawable.logan),
+        SuperHero("Batman", "Bruce Wayne", "DC", R.drawable.batman),
+        SuperHero("Thor", "Thor Odison", "Marvel", R.drawable.thor),
+        SuperHero("Flash", "Jay Garrick", "DC", R.drawable.flash),
+        SuperHero("Green Lantern", "Alan Scott", "DC", R.drawable.green_lantern),
+        SuperHero("Wonder Woman", "Princess Diana", "DC", R.drawable.wonder_woman)
+    )
 }
